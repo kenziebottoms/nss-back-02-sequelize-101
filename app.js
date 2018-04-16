f43ec69c2dae32229c385816193de569327b4f47
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-04-16 09:39:29 -0500

    patch shows/id

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-"use strict";
+'use strict';
 
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.set("models", require("./models"));
-const { User, Show, Director } = app.get("models");
+app.set('models', require('./models'));
+const { User, Show, Director } = app.get('models');
 
 // middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/shows", (req, res, next) => {
-  Show.findAll({ include: [{ model: Director, attributes: ["name"] }] })
+app.get('/shows', (req, res, next) => {
+  Show.findAll({ include: [{ model: Director, attributes: ['name'] }] })
     .then(shows => {
       res.status(200).json(shows);
     })
     .catch(err => next(err));
 });
-app.get("/shows/:id", (req, res, next) => {
+app.get('/shows/:id', (req, res, next) => {
   Show.findOne({
     raw: true,
     where: { id: req.params.id }
@@ -28,8 +28,17 @@ app.get("/shows/:id", (req, res, next) => {
     })
     .catch(err => next(err));
 });
+app.patch('/shows/:id', (req, res, next) => {
+  Show.find({where: {id: req.params.id}})
+    .then(obj => {
+      return obj.updateAttributes(req.body);
+    })
+    .then(obj => {
+      res.status(201).json(obj);
+    });
+});
 
-app.post("/favorites", ({ body: { UserId, ShowId } }, res, next) => {
+app.post('/favorites', ({ body: { UserId, ShowId } }, res, next) => {
   User.findById(UserId)
     .then(user => {
       console.log(user);
